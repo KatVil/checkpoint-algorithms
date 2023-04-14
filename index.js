bbb4b1b7f1ba2f03fded0a2314e9ec3a6300dc59
Katherine Vilinski-Mazur <Katherine.Vilinski-Mazur@skoltech.ru>
2023-04-14 20:39:45 +0300

    only 1st 2 tests

--- index.js
+++ index.js
@@ -1,9 +1,41 @@
 // Код для первого задания напиши в этой функции
-function crawlTree() {}
+function crawlTree(arr) {
+    let str = '';
+    for (let item = 0; item < arr.length; item++){
+        if(Array.isArray(arr[item])){
+            str+=crawlTree(arr[item]);
+        }
+        else{
+            str += arr[item];
+        }
+    }
+    return str;
+}
+console.log(crawlTree([
+    [[["П", "р"], "и"],
+      ["в", ["е", "т"]],
+    ],
+    [["и", "к"], "и"],
+  ]));
 
 // Код для второго задания напиши в этой функции
-function twoSum() {}
-
+function twoSum(arr, target) {
+    let arrIndexes = [];
+    let sum = 0;
+    let arrSliced = [];
+    for (let i = 0; i<arr.length; i++){
+        sum+=arr[i];
+        arrIndexes.push(i);
+        if (sum === target){
+            return arrIndexes;
+        }
+        else{
+            arrSliced = arr.slice(1);
+            return twoSum(arrSliced, target);
+        }
+    }
+}
+console.log(twoSum([3, 2, 4],6));
 // Всё, что ниже, нужно для тестов. Не изменяй этот код
 const functions = { crawlTree, twoSum };
 module.exports = functions;
